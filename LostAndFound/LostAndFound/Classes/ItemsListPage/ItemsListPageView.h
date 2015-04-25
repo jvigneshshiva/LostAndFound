415300ba6aa24008d769102f3bbb48cc85f22f9c
jvigneshshiva <jvshiva.cse@gmail.com>
2015-04-25 18:18:21 +0530

    Added New Page View File

---
+++ LostAndFound/LostAndFound/Classes/ItemsListPage/ItemsListPageView.h
@@ -0,0 +1,13 @@
+//
+//  ItemsListPageView.h
+//  LostAndFound
+//
+//  Created by Vignesh Shiva on 25/04/15.
+//  Copyright (c) 2015 VVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemsListPageView : UIView
+
+@end
---
+++ LostAndFound/LostAndFound/Classes/ItemsListPage/ItemsListPageView.m
@@ -0,0 +1,21 @@
+//
+//  ItemsListPageView.m
+//  LostAndFound
+//
+//  Created by Vignesh Shiva on 25/04/15.
+//  Copyright (c) 2015 VVS. All rights reserved.
+//
+
+#import "ItemsListPageView.h"
+
+@implementation ItemsListPageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
